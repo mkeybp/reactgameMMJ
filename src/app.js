454520ae86b0bad04e5f718fbdfaa33f371dfcb0
mkeybp <mikeypedersen@hotmail.com>
2021-04-08 10:37:27 +0200

    dd
    dd

---
+++ src/app.js
@@ -0,0 +1,12 @@
+
+// function TopScore(props) {
+//     return (
+//         <>
+//             <h1>first place: {props.first}</h1>
+//             <h1>second place: {props.second}</h1>
+//             <h1>third place: {props.third}</h1>
+//         </>
+//     )
+// }
+
+// export default TopScore;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './login';
+
+
+ReactDOM.render(
+  
+    <Login/>,
+ 
+    document.getElementById("root")
+);
