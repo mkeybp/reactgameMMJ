56f947df87fc44b56fb882847a74cf09914afc26
Lyhne1997 <55769759+Lyhne1997@users.noreply.github.com>
2021-04-14 14:04:57 +0200

    123

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
-import React from "react";
+// import React from "react";
 import Login from './login';
 import * as utils from './login';
 import useGameServer from "./useGameServer";
 import Map from "./map";
+import React, { useState } from 'react';
+
+
 
 let gameHubUrl = "http://jats.web.eadania.dk/gamehub"
 
@@ -11,18 +14,31 @@ class LoggedIn extends React.Component {
     super();
     this.state = { selection: "" };
     this.onLoggedin = this.onLoggedin.bind(this);
+    this.onDrawGround = this.drawMap.bind(this);
   };
 
   onLoggedin(token) {
     this.setState({ selection: token})
   }
+    drawMap(ground, clutter, moveables, effects) {
+    this.setState({selection: ground})
+    this.setState({selection: clutter})
+    this.setState({selection: moveables})
+    this.setState({selection: effects})
 
+
+}
   render() {
     return (
       this.state.selection !== "" ?
 
         <GameWindow
           token={this.state.selection}
+          ground={this.state.selection}
+          clutter={this.state.selection}
+          moveables={this.state.selection}
+          effects={this.state.selection}
+
         /> :
         <Login
         onLogin={this.onLoggedin}
@@ -31,6 +47,16 @@ class LoggedIn extends React.Component {
     );
   }
 }
+
+// function drawMap(ground, clutter, moveables, effects) {
+//     this.setState({selection: ground})
+//     this.setState({selection: clutter})
+//     this.setState({selection: moveables})
+//     this.setState({selection: effects})
+//     drawGround=(this.)
+// }
+
+
 function error(name) {
   // alert('Hello ' + name);
 }
@@ -42,29 +68,46 @@ function onConnectionClosed() {
 
 onConnectionClosed();
 
+
+
 function GameWindow(props) {
   const gameServer = useGameServer(gameHubUrl, props.token, onConnectionClosed);
-
+  const [chatMessage, setChatMessage] = useState("");
   gameServer.connect();
   gameServer.onEvent("WorldUpdate", response  => {
-    
+    //console.log(response);
+    //console.log(response.ground);
+    props.onDrawGround(response.ground);
+    props.onDrawGround(response.clutter);
+    props.onDrawGround(response.moveables);
+    props.onDrawGround(response.effects);
+
   });
-  gameServer.invoke("Chat", String => {
-    String = "123";
-  })
   gameServer.onEvent("ChatMessage", response  => {
     console.log(response);
   });
+  gameServer.invoke("Chat", chatMessage => {
+    console.log(chatMessage);
+  });
   return (
 
-    <>
-      <button onClick={props.onClose}>X</button>
-
-      <p>
-
-      </p>
-    </>
-  );
+      <div className='msg-footer'>
+        <form
+          className='message-form'
+          onSubmit={this.handleSendMessage}>
+          <div className='input-group'>
+            <input
+              type='text'
+              className='form-control message-input'
+              placeholder='Type something'
+              value={chatMessage}
+              onChange={event => this.setState({ chatMessage: event.target.value})}
+              required
+            />
+          </div>
+        </form>
+      </div>
+);
 }
 
 
